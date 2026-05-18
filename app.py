2b2deefacf96ec6acb2132bba67b533fd779f8a6
Radu <Radu@DESKTOP-T3V1AK1>
2026-05-18 15:22:18 +0300

    Step2

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Docker!"
+def home():
+    return "Home page"
+
+@app.route("/about")
+def about():
+    return "About page"
+
+@app.route("/api/users")
+def users():
+    return jsonify([
+        {"id": 1, "name": "Alice"},
+        {"id": 2, "name": "Bob"}
+    ])
+
+@app.route("/api/users/<int:user_id>")
+def user(user_id):
+    return jsonify({"id": user_id, "name": "Alice"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
